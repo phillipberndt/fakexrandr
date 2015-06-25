476a46d0db0671d6c31be2107afb45d1812ed334
Phillip Berndt <phillip.berndt@googlemail.com>
2015-06-25 13:44:39 +0200

    Added the functions that need to be overwritten to libxcb-randr.c

--- libxcb-randr.c
+++ libxcb-randr.c
@@ -43,3 +43,58 @@ static void _init() {
 	Overridden library functions to add the fake output
 */
 
+xcb_randr_get_screen_resources_cookie_t xcb_randr_get_screen_resources(xcb_connection_t *c, xcb_window_t window) {
+	printf("Get screen resources\n");
+	return _xcb_randr_get_screen_resources(c, window);
+}
+
+xcb_randr_get_screen_resources_cookie_t xcb_randr_get_screen_resources_unchecked(xcb_connection_t *c, xcb_window_t window) {
+	printf("Get screen resources unchecked\n");
+	return _xcb_randr_get_screen_resources_unchecked(c, window);
+}
+
+xcb_randr_get_screen_resources_reply_t *xcb_randr_get_screen_resources_reply(xcb_connection_t *c,
+		xcb_randr_get_screen_resources_cookie_t cookie, xcb_generic_error_t **e) {
+
+	printf("Get screen resources reply\n");
+	return _xcb_randr_get_screen_resources_reply(c, cookie, e);
+}
+
+
+
+xcb_randr_get_output_info_cookie_t xcb_randr_get_output_info(xcb_connection_t *c,
+		xcb_randr_output_t output, xcb_timestamp_t config_timestamp) {
+	printf("Get output info\n");
+	return _xcb_randr_get_output_info(c, output, config_timestamp);
+}
+
+xcb_randr_get_output_info_cookie_t xcb_randr_get_output_info_unchecked(xcb_connection_t *c,
+		xcb_randr_output_t output,
+		xcb_timestamp_t config_timestamp) {
+
+	printf("Get output info unchecked\n");
+	return _xcb_randr_get_output_info_unchecked(c, output, config_timestamp);
+}
+
+xcb_randr_get_output_info_reply_t * xcb_randr_get_output_info_reply(xcb_connection_t *c,
+		xcb_randr_get_output_info_cookie_t cookie, xcb_generic_error_t **e ) {
+
+	printf("Get output info reply\n");
+	return _xcb_randr_get_output_info_reply(c, cookie, e);
+}
+
+xcb_randr_get_crtc_info_cookie_t xcb_randr_get_crtc_info(xcb_connection_t *c, xcb_randr_crtc_t crtc, xcb_timestamp_t config_timestamp ) {
+
+	printf("Get crtc info\n");
+	return _xcb_randr_get_crtc_info(c, crtc, config_timestamp);
+}
+
+xcb_randr_get_crtc_info_cookie_t xcb_randr_get_crtc_info_unchecked(xcb_connection_t *c, xcb_randr_crtc_t crtc, xcb_timestamp_t config_timestamp ) {
+	printf("Get crtc info unchecked\n");
+	return _xcb_randr_get_crtc_info_unchecked(c, crtc, config_timestamp);
+}
+
+xcb_randr_get_crtc_info_reply_t * xcb_randr_get_crtc_info_reply(xcb_connection_t *c, xcb_randr_get_crtc_info_cookie_t cookie, xcb_generic_error_t **e ) {
+	printf("Get crtc info reply\n");
+	return _xcb_randr_get_crtc_info_reply(c, cookie, e);
+}
